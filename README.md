03656dbe39a50bd90455ecb19b21da2b0fabd44d
hujianglin <1262264261@qq.com>
2023-10-14 18:17:29 +0800

    feat: 新增query 和返回值转换、body 和返回值转换、全部转换按钮

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## api-to-interface
 
-项目引入ts之后，接口的返回值和参数也要有对应数据的interface定义，如果自己写太繁琐，而且容易出错，所以开发一个谷歌插件，利用递归遍历进行自动转换，支持嵌套数据
+项目引入 ts 之后，接口的返回值和参数也要有对应数据的 interface 定义，如果自己写太繁琐，而且容易出错，所以开发一个谷歌插件，利用递归遍历进行自动转换，支持嵌套数据
 数据：
 ![image](https://photo.zastatic.com/images/common-cms/it/20220106/1641458852002_704649_t.png)
 转换后：
@@ -8,58 +8,56 @@
 ```
 /** 接口地址 /
 export interface CheckTransactionIdData {
-  /** 提示信息 / 
-  errorMsg: string, 
+  /** 提示信息 /
+  errorMsg: string,
   /** 支付时间 /
-  payTime: number, 
+  payTime: number,
   /** 是否支付成功 /
-  success: boolean, 
+  success: boolean,
   /** 支付金额 /
-  totalAmount: number, 
+  totalAmount: number,
   /** 交易参考号 /
-  transactionId: string, 
-} 
+  transactionId: string,
+}
 
 export interface CheckTransactionId {
-  code: number, 
-  data: CheckTransactionIdData, 
+  code: number,
+  data: CheckTransactionIdData,
 }
 ```
 
- 
-
-
 ## 使用方式
 
-####  1、引入资源
-谷歌浏览器打开chrome://extensions/，  
-然后把解压的dist文件夹直接拖过去，就能加载插件了。打开yapi具体接口页面，右边会出现个浮层，点击就可以进行转换了  
+#### 1、引入资源
+
+谷歌浏览器打开 chrome://extensions/，
+然后把解压的 dist 文件夹直接拖过去，就能加载插件了。打开 yapi 具体接口页面，右边会出现个浮层，点击就可以进行转换了
 ![image](https://photo.zastatic.com/images/common-cms/it/20220106/1641459812069_323645_t.jpg)
 
-####   2、按钮功能
+#### 2、按钮功能
 
 返回值转换：
-返回值有两种格式，json字符串和json-schema，点击按钮转换为interface  
-Body参数转换：
-Body参数有三种格式，json字符串、json-schema、form，点击按钮转换为interface
-Query参数转换：
-Query参数只有一种格式，但是类型只能判断为string，点击按钮转换为interface
-
-
+返回值有两种格式，json 字符串和 json-schema，点击按钮转换为 interface
+Body 参数转换：
+Body 参数有三种格式，json 字符串、json-schema、form，点击按钮转换为 interface
+Query 参数转换：
+Query 参数只有一种格式，但是类型只能判断为 string，点击按钮转换为 interface
 
 ## 项目开发
 
 #### 1、安装依赖
-  npm install
 
-#### 2、开发模式(监听代码变化，生成content.js)
-  npm run dev
+npm install
 
-#### 3、编译(生成content.js，压缩版)
-  npm run build
+#### 2、开发模式(监听代码变化，生成 content.js)
 
-执行命令之后dist就是插件包
+npm run dev
 
+#### 3、编译(生成 content.js，压缩版)
+
+npm run build
+
+执行命令之后 dist 就是插件包
 
 ## 项目目录，注意及时更新
 
@@ -88,6 +86,14 @@ Query参数只有一种格式，但是类型只能判断为string，点击按钮
 ```
 
 ## 更新日志
-#### 2023.9.13 add 
+
+#### 2023.9.13 add
+
 考虑到不同团队的结尾分隔符号不一样，有的是分号，有的是逗号，添加输入框由开发自己选择，默认分号
 ![image](http://m.qpic.cn/psc?/V52unZiZ3Efn180CwBtQ1qG9922ap8gK/ruAMsa53pVQWN7FLK88i5jUxwHHE*QlZabaSyNSXMtJvqdhk*815PYod2MztVQuEB*mV*S7ciGxjO781TGbu39k3tvpY9eE4A0GG35eosrA!/b&bo=sgC8AAAAAAABBy4!&rf=viewer_4)
+
+#### 2023.10.14 add
+
+考虑到每次参数跟返回值都需要复制，所以新增 query 和返回值转换、body 和返回值转换、全部转换按钮
+
+![1697278455321](image/README/1697278455321.png)
--- src/lib/content.ts
+++ src/lib/content.ts
@@ -1,202 +1,303 @@
-import { copy, message, firstUpperCase } from '../utils/utils'
-import { CreateElement } from '../types/index'
-import { request } from './request'
-import { btnType } from '../utils/dict'
-import Compile from './compile'
-
+import { copy, message, firstUpperCase } from "../utils/utils";
+import { CreateElement } from "../types/index";
+import { request } from "./request";
+import { btnType } from "../utils/dict";
+import Compile from "./compile";
 
 // 插件显示的内容，需要内嵌在网页，所以采用appendChild
 export default class Content {
-  toTsCompile: any                 // 遍历转换对象
+  toTsCompile: any; // 遍历转换对象
+  prevValue: string; // 之前的值
 
   constructor() {
-    this.toTsCompile = new Compile(location.href)
+    this.toTsCompile = new Compile(location.href);
+    // 保存之前的值，可以组合
+    this.prevValue = "";
   }
 
   // 生成页面元素
   createElementFun(options: CreateElement) {
-    let $div = document.createElement(options.elem)
-    $div.innerText = options.innerText || ''
+    let $div = document.createElement(options.elem);
+    $div.innerText = options.innerText || "";
     // @ts-ignore
-    $div.classList = options.classList || ''
-    $div.type = options.type || ''
+    $div.classList = options.classList || "";
+    $div.type = options.type || "";
 
-    return $div
+    return $div;
   }
 
   // 页面加载时执行
   contentLoad(e: any) {
-    const target = e.target
-    
+    const target = e.target;
+
     // 主体框
-    const $btnWrapper = this.createElementFun({classList: 'to-ts' })
-    const $title = this.createBtnWrapper()
-    const $input = this.createSeparator()
-    const $queryBtn = this.createQueryBtn()
-    const $bodyBtn = this.createBodyBtn()
-    const $resBtn = this.createResBtn()
-   
-    $btnWrapper.appendChild($title)
-    $btnWrapper.appendChild($input)
-    $btnWrapper.appendChild($queryBtn)
-    $btnWrapper.appendChild($bodyBtn)
-    $btnWrapper.appendChild($resBtn)
-    target.body.appendChild($btnWrapper)
+    const $btnWrapper = this.createElementFun({ classList: "to-ts" });
+    const $title = this.createBtnWrapper();
+    const $input = this.createSeparator();
+    const $queryBtn = this.createQueryBtn();
+    const $bodyBtn = this.createBodyBtn();
+    const $resBtn = this.createResBtn();
+    const $queryAndResBtn = this.createQueryAndResBtn();
+    const $bodyAndResBtn = this.createBodyAndResBtn();
+    const $allBtn = this.createallBtn();
+
+    $btnWrapper.appendChild($title);
+    $btnWrapper.appendChild($input);
+    $btnWrapper.appendChild($queryBtn);
+    $btnWrapper.appendChild($bodyBtn);
+    $btnWrapper.appendChild($resBtn);
+    $btnWrapper.appendChild($queryAndResBtn);
+    $btnWrapper.appendChild($bodyAndResBtn);
+    $btnWrapper.appendChild($allBtn);
+    target.body.appendChild($btnWrapper);
   }
 
   // 插件框标题
   createBtnWrapper() {
     const $title = this.createElementFun({
-      elem: 'h4', 
-      classList: 'to-ts-title', 
-      innerText: '接口转为ts interface'
-    })
-    
-    return $title
+      elem: "h4",
+      classList: "to-ts-title",
+      innerText: "接口转为ts interface",
+    });
+
+    return $title;
   }
 
-   // 类型分隔符，比如分号，逗号，空， '；' ' ，' '' 
-   createSeparator() {
+  // 类型分隔符，比如分号，逗号，空， '；' ' ，' ''
+  createSeparator() {
     const $input = this.createElementFun({
-      elem: 'input', 
-      type: 'text',
-      classList: 'separator', 
-    })
+      elem: "input",
+      type: "text",
+      classList: "separator",
+    });
     const $inputWrapper = this.createElementFun({
-      elem: 'div', 
-      classList: 'separator-wrapper', 
-      innerText: '每行分隔符'
-    })
-    $inputWrapper.appendChild($input)
-    $input.value = this.toTsCompile.separator
+      elem: "div",
+      classList: "separator-wrapper",
+      innerText: "每行分隔符",
+    });
+    $inputWrapper.appendChild($input);
+    $input.value = this.toTsCompile.separator;
     $input.onblur = (e: any) => {
       this.toTsCompile.separator = e.target.value;
-    }
+    };
 
-    return $inputWrapper
+    return $inputWrapper;
   }
 
   // 返回值转换按钮
   createResBtn() {
     const $resBtn = this.createElementFun({
-      elem: 'button', 
-      classList: 'to-ts-res-btn', 
-      innerText: '返回值转换'
-    })
-   
-    $resBtn.onclick =  () => {
-      this.clickCallBack($resBtn, 'resBtn')
-    }
+      elem: "button",
+      classList: "to-ts-res-btn",
+      innerText: "返回值转换",
+    });
 
-    return $resBtn
+    $resBtn.onclick = () => {
+      this.clickCallBack($resBtn, "resBtn");
+    };
+
+    return $resBtn;
   }
-
 
   // Body参数按钮
   createBodyBtn() {
     // Body参数
     const $bodyBtn = this.createElementFun({
-      elem: 'button', 
-      classList: 'to-ts-req-btn', 
-      innerText: 'Body参数转换'
-    })
+      elem: "button",
+      classList: "to-ts-req-btn",
+      innerText: "Body参数转换",
+    });
 
     $bodyBtn.onclick = () => {
-      this.clickCallBack($bodyBtn, 'bodyBtn')
-    }
+      this.clickCallBack($bodyBtn, "bodyBtn");
+    };
 
-    return $bodyBtn
+    return $bodyBtn;
   }
 
   // 生成query参数按钮
   createQueryBtn() {
     // Query参数
     const $queryBtn = this.createElementFun({
-      elem: 'button', 
-      classList: 'to-ts-query-btn', 
-      innerText: 'Query参数转换'
-    })
-  
+      elem: "button",
+      classList: "to-ts-query-btn",
+      innerText: "Query参数转换",
+    });
+
     $queryBtn.onclick = () => {
-      this.clickCallBack($queryBtn, 'queryBtn')
-    }
-    
-    return $queryBtn
+      this.clickCallBack($queryBtn, "queryBtn");
+    };
+
+    return $queryBtn;
+  }
+
+  // query跟res转换按钮
+  createQueryAndResBtn() {
+    const $queryAndResBtn = this.createElementFun({
+      elem: "button",
+      classList: "to-ts-req-btn and",
+      innerText: "query和返回值转换",
+    });
+
+    $queryAndResBtn.onclick = () => {
+      this.clickCallBack(
+        $queryAndResBtn,
+        "queryAndResBtn",
+        "query和返回值转换"
+      );
+    };
+
+    return $queryAndResBtn;
+  }
+
+  // body跟res转换按钮
+  createBodyAndResBtn() {
+    const $bodyAndResBtn = this.createElementFun({
+      elem: "button",
+      classList: "to-ts-req-btn and-body",
+      innerText: "body和返回值转换",
+    });
+
+    $bodyAndResBtn.onclick = async () => {
+      this.clickCallBack($bodyAndResBtn, "bodyAndResBtn", "body和返回值转换");
+    };
+
+    return $bodyAndResBtn;
+  }
+
+  // 有可能query，body, res都有(别问为什么，问后台)
+  createallBtn() {
+    const $allBtn = this.createElementFun({
+      elem: "button",
+      classList: "to-ts-req-btn and-all",
+      innerText: "全部转换",
+    });
+
+    $allBtn.onclick = () => {
+      this.clickCallBack($allBtn, "allBtn", "全部转换");
+    };
+
+    return $allBtn;
   }
 
   // 按钮点击后回调，生成interface返回
-  clickCallBack(element: any, type: string) {
-    element.innerText = '转换中...'
-      request().then((result: any) => {
-        try {
-          result = JSON.parse(result).data
-          const title = result.title // 接口名称
-          let resBody = result[btnType[type].field]
+  async clickCallBack(element: any, type: string, innerText: string = "") {
+    element.innerText = "转换中...";
+    // 清空旧值
+    this.prevValue = "";
+    let result: any = await request();
 
-          // 如果返回是个json字符串，需要解析
-          if (typeof resBody === 'string') {
-            // 不能JSON.parse，因为存在注释, rollup强烈反对使用eval，用new Function代替
-            resBody = new Function('return ' + result[btnType[type].field])()
+    if (type === "queryAndResBtn") {
+      this.callbackFunc(element, "queryBtn", false, innerText, result);
+      this.callbackFunc(element, "resBtn", true, innerText, result);
+    } else if (type === "bodyAndResBtn") {
+      this.callbackFunc(element, "bodyBtn", false, innerText, result);
+      this.callbackFunc(element, "resBtn", true, innerText, result);
+    } else if (type === "allBtn") {
+      this.callbackFunc(element, "queryBtn", false, innerText, result);
+      this.callbackFunc(element, "bodyBtn", false, innerText, result);
+      this.callbackFunc(element, "resBtn", true, innerText, result);
+    } else {
+      this.callbackFunc(element, type, false, innerText, result);
+    }
+  }
+
+  // 接口回调
+  callbackFunc(
+    element: any,
+    type: string,
+    isAnd: boolean = false,
+    innerText: string = "",
+    result: any
+  ) {
+    try {
+      result = JSON.parse(result).data;
+      const title = result.title; // 接口名称
+      let resBody = result[btnType[type].field];
+
+      // 如果返回是个json字符串，需要解析
+      if (typeof resBody === "string") {
+        // 不能JSON.parse，因为存在注释, rollup强烈反对使用eval，用new Function代替
+        resBody = new Function("return " + result[btnType[type].field])();
+      }
+
+      let compileType = "";
+      // 老接口有.do，需要去掉
+      const splitPath = result.path.replace(".do", "").split("/");
+      // 获取接口名，生成的interface名跟接口名绑定
+      const apiPath = splitPath[splitPath.length - 1];
+      let interfaceName = apiPath;
+
+      // 如果接口存在-,则去掉，并变成驼峰命名
+      if (apiPath.indexOf("-") > -1) {
+        const arr = apiPath.split("-");
+
+        arr.forEach((item: string, index: number) => {
+          if (index > 0) {
+            arr[index] = firstUpperCase(item);
           }
-         
-          let compileType = ''
-          // 老接口有.do，需要去掉
-          const splitPath = result.path.replace('.do', '').split('/')
-          // 获取接口名，生成的interface名跟接口名绑定
-          const apiPath = splitPath[splitPath.length - 1]
-          let interfaceName = apiPath
+        });
+        interfaceName = arr.join("");
+      }
 
-          // 如果接口存在-,则去掉，并变成驼峰命名
-          if (apiPath.indexOf('-') > -1) {
-            const arr = apiPath.split('-')
-            
-            arr.forEach((item: string, index: number) => {
-              if (index > 0) {
-                arr[index] = firstUpperCase(item)
-              }
-            })
-            interfaceName = arr.join('')
-          } 
-
-          // 返回值跟参数分开做处理
-          if (type === 'resBtn') {
-            interfaceName += 'Res'
-            if (result.res_body_is_json_schema) {
-              // 当res_body_type字段为json时取resBody.properties，为raw时不做处理，就是resBody
-              if (result.res_body_type === 'json') {
-                resBody = resBody.properties
-                compileType = 'json_schema'
-              }
-            }
-          } else if (type === 'bodyBtn') {
-            interfaceName += 'Body'
-            // post请求有两种，一种是form，一种是json,
-            if (result.req_body_type === 'form') {
-              resBody = result.req_body_form
-              compileType = 'form'
-            } else {  
-              // json有两种，一种是res_body_is_json_schema为true，一种是false
-              if (result.req_body_is_json_schema) {
-                resBody = resBody.properties
-                compileType = 'json_schema'
-              }
-            }
-          } else if (type === 'queryBtn') {
-            interfaceName += 'Query'
-            compileType = 'query'
+      // 返回值跟参数分开做处理
+      if (type === "resBtn") {
+        interfaceName += "Res";
+        if (result.res_body_is_json_schema) {
+          // 当res_body_type字段为json时取resBody.properties，为raw时不做处理，就是resBody
+          if (result.res_body_type === "json") {
+            resBody = resBody.properties;
+            compileType = "json_schema";
           }
-
-          const value = this.toTsCompile.compile(resBody, interfaceName, compileType, title)
-          
-          copy(value)
-          element.innerText = btnType[type].innerText
-          message({ text: '复制成功', type: 'success' })
-        } catch(err) {
-          console.error(err)
-          element.innerText = btnType[type].innerText
-          message({ text: `生成失败, 请检查是否有${btnType[type].innerText}`, type: 'error' })
         }
-      })
+      } else if (type === "bodyBtn") {
+        interfaceName += "Body";
+        // post请求有两种，一种是form，一种是json,
+        if (result.req_body_type === "form") {
+          resBody = result.req_body_form;
+          compileType = "form";
+        } else {
+          // json有两种，一种是res_body_is_json_schema为true，一种是false
+          if (result.req_body_is_json_schema) {
+            resBody = resBody.properties;
+            compileType = "json_schema";
+          }
+        }
+      } else if (type === "queryBtn") {
+        interfaceName += "Query";
+        compileType = "query";
+      }
+
+      const value = this.toTsCompile.compile(
+        resBody,
+        interfaceName,
+        compileType,
+        title
+      );
+
+      // 是否需要之前的数据
+      if (isAnd) {
+        copy(this.prevValue + value);
+        element.innerText = innerText;
+      } else {
+        copy(value);
+        element.innerText = btnType[type].innerText;
+      }
+
+      this.prevValue = this.prevValue + value;
+      message({ text: "复制成功", type: "success" });
+    } catch (err) {
+      console.error(err);
+      this.prevValue = "";
+      if (isAnd) {
+        element.innerText = innerText;
+      } else {
+        element.innerText = btnType[type].innerText;
+      }
+      message({
+        text: `生成失败, 请检查是否有${btnType[type].innerText}`,
+        type: "error",
+      });
+    }
   }
 }
-
